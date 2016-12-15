08602e4f3bfcd42f759e2d67d8b67acb858c4f3e
stompesi <stompesi@gmail.com>
2016-12-15 12:49:56 +0900

    BOJ - 간단한문제 - 출력

---
+++ BOJ/간단한문제/출력/Main11719.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main11719 {
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		while(sc.hasNextLine()) {
+			String string = sc.nextLine();
+			System.out.println(string);
+		}
+	}
+}
